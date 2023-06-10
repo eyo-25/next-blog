dd3ab050f25b0d3d323eba7de0424909ca7b1c2e
eyo25 <qutuzm@naver.com>
2023-06-10 23:58:32 +0900

    test

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,15 +1,8 @@
-"use client"
-
-import PostsGrids from "@/components/PostsGrids"
-import { useState } from "react"
-
 export default function PostsPage(){
     const categories = ["All Posts", "location", "traditional", "food"]
-    const [selected, setSelected] = useState();
 
     return (
         <div>
-            {/* <PostsGrids posts={}/> */}
             <ul>
                 {categories.map(category => <li key={category}>{category}</li>)}
             </ul>
